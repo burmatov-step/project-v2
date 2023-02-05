985ef300690ea256cd75f8a89d630e3c3c1290ef
burmatov-step <58148434+burmatov-step@users.noreply.github.com>
2023-02-05 19:43:58 +0500

    10 less

--- config/build/buildResolve.ts
+++ config/build/buildResolve.ts
@@ -1,7 +1,12 @@
+import { BuildOptions } from './types/config';
 import webpack from 'webpack';
 
-export function buildResolve(): webpack.ResolveOptions {
+export function buildResolve(options: BuildOptions): webpack.ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {} 
     }
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import './styles/index.scss'
 import { Link } from "react-router-dom";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { Suspense, useContext, useState } from "react";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
 
 
 
@@ -23,8 +22,8 @@ const App = () => {
             </Link>
             <Suspense fallback={<div>...Loading</div>}>
                 <Routes>
-                    <Route path={'/'} element={<MainPageAsync />} />
-                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPage />} />
+                    <Route path={'/about'} element={<AboutPage />} />
                 </Routes>
             </Suspense>
         </div>
