9d2f84e79850289803e7b1e404d5725cdd34f41d
burmatov-step <58148434+burmatov-step@users.noreply.github.com>
2023-02-07 21:40:33 +0500

    13 less

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,35 +3,52 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
 
-export function buildLoaders({isDev}:BuildOptions):webpack.RuleSetRule[] {
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-         isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          {
-            loader: "css-loader",
-            options:{
-                modules: {
-                    auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-                },
-                
-            }
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
           },
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      }
-    return [
-        typescriptLoader,
-        cssLoader
-    ]
+
+        }
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  }
+  return [
+    svgLoader,
+    fileLoader,
+    typescriptLoader,
+    cssLoader,
+
+  ]
 }
